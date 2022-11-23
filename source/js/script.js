c6c782c31ff2cfb9da8997134ce34262346b3f63
Fedorov Mitriy <fanzholl@yandex.ru>
2022-11-23 03:12:43 +0300

    Start display is done.
    Added animations and new block.

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,15 @@
 function openMenu() {
+    burger.style.display = 'none';
     menu.style.transform = 'translateX(0%)';
 }
 
 function closeMenu() {
     menu.style.transform = 'translateX(100%)'
+    burger.style.display = 'block';
 }
 
 let menu = document.querySelector('.menu')
 let close = document.querySelector('.close');
-let humsteron = document.querySelector('.burger_container');
-humsteron.addEventListener('click', openMenu);
+let burger = document.querySelector('.burger_container');
+burger.addEventListener('click', openMenu);
 close.addEventListener('click', closeMenu);
